2160aee6eed1483d0ecb49ea7a739715406d82ea
gunkim <gunkim.dev@gmail.com>
2021-12-20 16:35:41 +0900

    TIL 커스텀

--- theme.config.js
+++ theme.config.js
@@ -1,12 +1,14 @@
 export default {
-  github: 'https://github.com/shuding/nextra',
+  github: 'https://github.com/gunkim',
   docsRepositoryBase: 'https://github.com/shuding/nextra/blob/master',
   titleSuffix: ' – Nextra',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
+      <span className="mr-2 font-extrabold hidden md:inline">
+        Today Gun Learned
+      </span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        The Next.js Static Site Generator
+        오늘 내가 배운 것
       </span>
     </>
   ),
